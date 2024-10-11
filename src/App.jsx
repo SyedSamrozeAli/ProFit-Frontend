2dfff0637593fa67fec864cf307ad281853b5a52
shaheer beig <shaheerbeig@gmail.com>
2024-10-11 15:14:47 +0500

    defined routing dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,38 @@ function App() {
       element: <LoginPage />
     },
     {
-      path: '/user',
-      element: <DashboardLayout />,
-      // children:[
-      //   {
-      //     path:"/dashboard"
-      //   }
-      // ]
+      path: '/admin',
+      element: <DashboardLayout/>,
+      children:[
+        {
+          path:"dashboard",
+          element:<div className='pink'>Dashboard Content</div>
+        },
+        {
+          path:"members",
+          element:<div>hellsbdnsadnmsao</div>
+        },
+        {
+          path:"trainers",
+          element:<div>hesdsadlsallo</div>
+        },
+        {
+          path:"attendance",
+          element:<div>headssadasllo</div>
+        },
+        {
+          path:"inventory",
+          element:<div>helasdasdaslo</div>
+        },
+        {
+          path:"finance",
+          element:<div>helladsadao</div>
+        },
+        {
+          path:"settings",
+          element:<div>hellocdafda</div>
+        }
+      ]
     }
   ])
   return (
