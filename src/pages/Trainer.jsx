3942288b80fce53aff3eca7b6a695c4cefbbfa7a
shaheer beig <shaheerbeig@gmail.com>
2024-10-19 00:12:37 +0500

    updated trainer.jsx

--- src/pages/Trainer.jsx
+++ src/pages/Trainer.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Search from "/images/search.png";
+import DisplayTrainer from "../components/DisplayTrainer";
 
 function Trainer() {
 
@@ -15,17 +15,7 @@ function Trainer() {
           className='bg-red-600 hover:bg-red-700 rounded-lg px-4 py-2 text-white text-sm '
           onClick={() => navigate("/admin/trainers/addtrainers")}>+  Add Trainer</button>
         </div>
-        <div className='bg-gray-50 h-16 rounded-lg flex items-center '>
-          <div className='px-6 md:px-10 lg:px-16 flex'>
-            <input 
-            className="p-1 md:p-1 lg:p-2 sm:w-40 md:w-60 lg:w-80 rounded-lg bg-no-repeat bg-right cursor-pointer outline-none"
-            type='text'
-            placeholder='Search'
-            style={{ backgroundImage: `url(${Search})`,backgroundSize: '20px 20px'  }}>
-            </input>
-            
-          </div>    
-        </div>
+        <DisplayTrainer />
       </div>
     </div>
     </>
