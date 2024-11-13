4424a2791c6833fa1c27c9279282b403aa14bd1a
shaheer beig <shaheerbeig@gmail.com>
2024-11-13 20:26:22 +0500

    member file created

---
+++ src/pages/Member.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import DisplayTrainer from "../components/Trainers/DisplayTrainer";
+import NavBar from "../components/NavBar";
+
+function Member() {
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+    <NavBar title="Member" />
+    <div className='w-full h-screen bg-white'>
+      <div className='mx-6 md:mx-10 lg:mx-20 my-4'>
+        <div className='flex justify-between items-center mb-5'>
+          <p className='font-semibold text-lg'>Members Page</p>
+          <button 
+          className='bg-red-600 hover:bg-red-700 rounded-lg px-4 py-2 text-white text-sm '
+          onClick={() => navigate("/admin/members/addmembers")}>+  Add Member</button>
+        </div>
+        {/* <DisplayTrainer /> */}
+      </div>
+    </div>
+    </>
+  )
+}
+export default Member
