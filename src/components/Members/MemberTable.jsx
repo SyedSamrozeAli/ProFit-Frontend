e3d60f3605cda1e06009a6b43cc36a015f484914
Syed Samroze Ali <samroze3@gmail.com>
2024-11-25 22:09:56 +0500

    Minor changes in styling

--- src/components/Members/MemberTable.jsx
+++ src/components/Members/MemberTable.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import DataTable from 'react-data-table-component';
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
 import { MdDeleteOutline, MdSecurityUpdate } from "react-icons/md";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function MemberTable({ memberData = [], handleDeleteField }) {
-  
   const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleUpdateField = (id) => {
     navigate(`/admin/member/update/${id}`);
@@ -21,16 +20,22 @@ function MemberTable({ memberData = [], handleDeleteField }) {
     member_id: member.member_id,
     member_name: member.member_name,
     membership_Type: member.membership_type,
-    start_date: member.start_date?.split(" ")[0] || '',
+    start_date: member.start_date?.split(" ")[0] || "",
     trainer_name: member.trainer_name,
     status: "Active",
     action: (
       <div className="flex space-x-2">
-        <button onClick={() => handleDeleteField(member.member_id)} className='p-2'>
-          <MdDeleteOutline className='text-xl' />
+        <button
+          onClick={() => handleDeleteField(member.member_id)}
+          className="p-2"
+        >
+          <MdDeleteOutline className="text-xl" />
         </button>
-        <button onClick={() => handleUpdateField(member.member_id)} className='p-2 '>
-          <MdSecurityUpdate className='text-xl' />
+        <button
+          onClick={() => handleUpdateField(member.member_id)}
+          className="p-2 "
+        >
+          <MdSecurityUpdate className="text-xl" />
         </button>
       </div>
     ),
@@ -39,76 +44,87 @@ function MemberTable({ memberData = [], handleDeleteField }) {
   const column = [
     {
       name: "Member ID",
-      selector: row => row.member_id,
+      selector: (row) => row.member_id,
       sortable: true,
-      minWidth: '150px'
+      minWidth: "150px",
     },
     {
       name: "Name",
-      selector: row => row.member_name,
+      selector: (row) => row.member_name,
       sortable: true,
-      minWidth: '150px'
+      minWidth: "150px",
     },
     {
       name: "Membership Type",
-      selector: row => row.membership_Type,
+      selector: (row) => row.membership_Type,
       sortable: true,
-      minWidth: '80px',
-      cell: row => (
-        <span style={{
-          color: row.membership_Type === 'Premium' ? '#FFD700' : '#00796B',
-        }}>
+      minWidth: "80px",
+      cell: (row) => (
+        <span
+          style={{
+            color: row.membership_Type === "Premium" ? "#FFD700" : "#00796B",
+          }}
+        >
           {row.membership_Type}
         </span>
       ),
     },
     {
       name: "Admission Date",
-      selector: row => row.start_date,
+      selector: (row) => row.start_date,
       sortable: true,
-      minWidth: '120px'
+      minWidth: "120px",
     },
     {
       name: "Trainer Name",
-      selector: row => row.trainer_name ? row.trainer_name : "Not Available",
+      selector: (row) =>
+        row.trainer_name ? row.trainer_name : "Not Available",
       sortable: true,
-      minWidth: '100px'
+      minWidth: "100px",
     },
     {
       name: "Status",
-      selector: row => row.status,
-      cell: row => (
-        <span style={{
-          color: row.status === 'Active' ? '#0D7300' : '#EB0707',
-          background: row.status === 'Active' ? '#DFFFD7' : '#FFE8E8',
-          padding: '4px',
-          borderRadius: '6px',
-          display: 'inline-block'
-        }}>
+      selector: (row) => row.status,
+      cell: (row) => (
+        <span
+          style={{
+            color: row.status === "Active" ? "#0D7300" : "#EB0707",
+            background: row.status === "Active" ? "#DFFFD7" : "#FFE8E8",
+            padding: "4px",
+            borderRadius: "6px",
+            display: "inline-block",
+          }}
+        >
           {row.status}
         </span>
       ),
-      minWidth: '120px'
+      minWidth: "120px",
     },
     {
       name: "Action",
-      selector: row => row.action,
-      minWidth: '150px',
-      cell: row => <div className="flex justify-center space-x-2">{row.action}</div>
-    }
+      selector: (row) => row.action,
+      minWidth: "150px",
+      cell: (row) => (
+        <div className="flex justify-center space-x-2">{row.action}</div>
+      ),
+    },
   ];
 
   const customStyles = {
     rows: {
       style: {
-        borderBottom: '1px solid #E5E7EB',
+        borderBottom: "1px solid #E5E7EB",
+        "&:hover": {
+          backgroundColor: "#f3f4f6",
+        },
+        cursor: "pointer",
       },
     },
     headCells: {
       style: {
-        fontWeight: 'bold',
-        borderBottom: '2px solid #E5E7EB',
-        backgroundColor: '#F3F4F6',
+        fontWeight: "bold",
+        borderBottom: "2px solid #E5E7EB",
+        backgroundColor: "#F3F4F6",
       },
     },
   };
@@ -116,7 +132,6 @@ function MemberTable({ memberData = [], handleDeleteField }) {
   const handleRowClick = (row) => {
     navigate(`/admin/member/details/${row.member_id}`);
   };
-
 
   return (
     <div className="p-4 border border-gray-300 rounded-lg shadow-sm overflow-x-auto">
