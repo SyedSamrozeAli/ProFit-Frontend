2638943d7490b7f9de0370564fdde338251a7198
Syed Samroze Ali <samroze3@gmail.com>
2024-12-02 00:28:15 +0500

    Fiannce report genration page added

---
+++ src/pages/FinanceReportGenerate.jsx
@@ -0,0 +1,10 @@
+import FinanceReportForm from "../components/Forms/FinanceReportForm.jsx";
+
+export default function FinanceReportPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Finance Report Generation</h1>
+      <FinanceReportForm />
+    </div>
+  );
+}
