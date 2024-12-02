f8489ebf3ce6429e040d8cb95925011559f0f668
Syed Samroze Ali <samroze3@gmail.com>
2024-12-02 23:58:39 +0500

    Finance report page added

--- src/pages/FinanceReportGenerate.jsx
+++ src/pages/FinanceReportGenerate.jsx
@@ -4,11 +4,11 @@ import NavBar from "../components/NavBar.jsx";
 export default function FinanceReportPage() {
   return (
     <>
-    <NavBar title="Finance Report Generate"/>
-    <div className="container mx-auto p-4 px-12">
-      <h1 className="text-2xl font-bold mb-4 ">Finance Report Generation</h1>
-      <FinanceReportForm />
-    </div>
+      <NavBar title="Manage Finance" />
+      <div className="container mx-auto p-4 px-12">
+        <h1 className="text-2xl font-bold mb-4 ">Finance Report Generation</h1>
+        <FinanceReportForm />
+      </div>
     </>
   );
 }
