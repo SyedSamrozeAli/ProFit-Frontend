88716c2313736a512e1e48900559c3169e73add7
shaheer beig <shaheerbeig@gmail.com>
2024-12-04 21:46:11 +0500

    added trainer image

--- src/components/Forms/TrainerForm.jsx
+++ src/components/Forms/TrainerForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect , useState} from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -12,16 +12,26 @@ function TrainerForm({
   btnText,
 }) {
   const token = localStorage.getItem("token");
-
+  const [selectedImage, setSelectedImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
   // Handle form submission
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    const dataForm = new FormData();
+    Object.keys(trainer).forEach((key) => {
+      dataForm.append(key, trainer[key]);
+    });
+    if (selectedImage) {
+      dataForm.append("trainer_profile_image", selectedImage);
+    }
+
     axios({
       method: requestType,
       url: URL,
-      data: trainer,
+      data: dataForm,
       headers: {
         Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
       },
     })
       .then((response) => {
@@ -32,7 +42,8 @@ function TrainerForm({
         });
       })
       .catch((error) => {
-        console.log(error.response.data.message);
+        console.log(error);
+        // console.log(error.response.data.message);
         toast.error(error.response.data.message);
       });
   };
@@ -46,17 +57,45 @@ function TrainerForm({
     }));
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      console.log("Selected file:", file);
+      setSelectedImage(file);
+
+      // Use FileReader for image preview
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreviewImage(reader.result); // Set the preview image as base64 data URL
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setSelectedImage(null);
+      setPreviewImage("");
+    }
+  };
+
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="flex flex-col md:flex-row md:space-x-10">
-        <div className="flex-shrink-0 mb-6 md:mb-0">
+      <div className="flex-shrink-0 mb-6 md:mb-0 text-center">
           <img
-            src={profileImage}
+            src={previewImage || profileImage}
             alt="profile"
-            className="w-32 h-32 md:w-48 md:h-48 object-cover rounded-full border-2 border-gray-300"
+            className="w-32 h-32 md:w-48 md:h-48 object-cover rounded-full border-2 border-gray-300 mx-auto"
           />
-          <button type="button" className="mt-4 text-blue-500">
-            Upload Photo
+
+          <label>Upload Profile Image</label>
+          <input
+            type="file"
+            name="trainer_profile_image"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="mt-2 p-2 w-full text-sm text-gray-500 border rounded-md"
+            // required
+          />
+          <button type="submit" className="mt-4 text-blue-500">
+            <i className="fas fa-camera"></i>
           </button>
         </div>
 
